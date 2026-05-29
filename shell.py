c4b449b7ab890b53dfc7246cf053f7c5acbee22f
SamVJulius <samsonvjulius@gmail.com>
2026-05-29 23:32:28 +0530

    Added semi-colon dilimeter

--- shell.py
+++ shell.py
@@ -2,8 +2,13 @@ import basic
 
 while True:
     commnand = input('bhal-lang > ')
+    if commnand.strip() == "":
+        continue
     result, error = basic.run("input file", commnand)
     if error:
         print(error.as_string())
     elif result:
-        print(repr(result))
+        if len(result.elements) == 1:
+            print(repr(result.elements[0]))
+        else:
+            print(repr(result))
